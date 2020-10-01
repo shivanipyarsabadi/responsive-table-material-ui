fe0445254f24b190530d4bbfd7a4c083699e07d5
shivanipyarsabadi <shivani.pyarsabadi@gmail.com>
2020-10-01 10:45:35 +0200

    Updates

---
+++ precache-manifest.3fb6939a1f0e5f6af6cbb7233bc723ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bf53ec8e6efdc73b31d8b82f322bc7",
+    "url": "/responsive-table-material-ui/index.html"
+  },
+  {
+    "revision": "0f293cf4a4dd6c7e70ff",
+    "url": "/responsive-table-material-ui/static/css/main.a483ce80.chunk.css"
+  },
+  {
+    "revision": "6b0b6f21e0e0e8872148",
+    "url": "/responsive-table-material-ui/static/js/2.6e16f7d9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/responsive-table-material-ui/static/js/2.6e16f7d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f293cf4a4dd6c7e70ff",
+    "url": "/responsive-table-material-ui/static/js/main.738251bb.chunk.js"
+  },
+  {
+    "revision": "13fb696aba0558f03e70",
+    "url": "/responsive-table-material-ui/static/js/runtime-main.e37eeb48.js"
+  }
+]);
